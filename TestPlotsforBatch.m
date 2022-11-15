d41e645a2882d1566ea3f6448c5b5a3567d5c951
Ross Christopher Nichols <Rosscnichols@gmail.com>
2022-11-15 15:36:37 -0800

    Dive Stats
    Added a bunch of new variables and adjusted for batch processing. It may be good to test!

--- TestPlotsforBatch.m
+++ TestPlotsforBatch.m
@@ -1,6 +1,11 @@
 % Test Plots
 
-index = 1658;
-plot(-p(Start_Cue_Idx(index):End_Cue_Idx(index)))
-xline(Ascent_Cue(index)*fs,'r')
-xline(Descent_Cue(index)*fs,'b')
+index = 1;
+plot(-prh.p(Start_Cue_Idx(index):End_Cue_Idx(index)))
+xline(Ascent_Cue(index)*prh.fs,'r')
+xline(Descent_Cue(index)*prh.fs,'b')
+
+
+%% plot(-prh.p)
+xline(Ascent_idx,'r')
+xline(Descent_idx,'b')
