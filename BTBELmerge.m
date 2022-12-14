3b39007b8262678f22991d96bdb5efabe635824c
Ross Christopher Nichols <Rosscnichols@gmail.com>
2022-12-14 10:18:53 -0800

    Added notes to BTBELmerge
    Added notes for use into script

--- BTBELmerge.m
+++ BTBELmerge.m
@@ -1,4 +1,21 @@
 % Merge BTBEL Export Tables
+%     The BTBELmerge script can be opened in Matlab (any 2022 version, although it probably works in all versions).
+%     
+%     What does it do:
+% 	    -This script merges the excel files exported from the ipad into a single worksheet
+%     
+%     What is required:
+% 	    - All excel export files need to be in the same folder
+% 	    - Matlab must be installed on computer
+%     
+%     How to Run:
+% 	    - After your excel files are exported from the ipad and downloaded to your computer
+% 	    - Open the script in Matlab (double click script)
+% 	    - Press the "Run" play button in Matlab
+% 	    - A new window will open, in this window select all (you can select multiple files) excel export files you wish to merge
+% 	    - Press "open"
+% 	    - The script will run and a file called "BTBELmerge.xlsx" will be created in the same folder as your export files.
+
 function BTBELmerge
 
     % Set Export Filename
